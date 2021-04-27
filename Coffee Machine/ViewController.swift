92ce4ae6d5d68da6afd0af9e56fb4d5810673b89
Valentyn Bokhonov <v.bokhonov@serverpipe.com>
2021-04-27 15:24:56 +0300

    version 2: UI added, error handler implemented

--- Coffee Machine/ViewController.swift
+++ Coffee Machine/ViewController.swift
@@ -8,22 +8,53 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var infoDisplayLable: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-//        addMilk()
-//        addWater()
-//        addCoffee()
-//        addWater()
-//        addWater()
-//        clearGarbage()
         
-        drinkPrepare(drink: .cappuchino)
+        infoDisplayLable.layer.borderWidth = 1
+        infoDisplayLable.layer.cornerRadius = 10
+        infoDisplayLable.text = ""
         
         print("Water: \(waterTank) | Milk: \(milkTank) | Coffee: \(coffeeTank) | Garbage: \(garbageTank)")
     }
 
+    //Drink buttons
     
-
+    @IBAction func prepareEspressoButton(_ sender: UIButton) {
+        prepareDrink(drink: .espresso)
+        infoDisplayLable.text = errorText
+    }
+    @IBAction func prepareAmericanoButton(_ sender: UIButton) {
+        prepareDrink(drink: .americano)
+        infoDisplayLable.text = errorText
+    }
+    @IBAction func prepareCappuchinoButton(_ sender: UIButton) {
+        prepareDrink(drink: .cappuchino)
+        infoDisplayLable.text = errorText
+    }
+    @IBAction func prepareTeaButton(_ sender: UIButton) {
+        prepareDrink(drink: .tea)
+        infoDisplayLable.text = errorText
+    }
+    
+    // Service buttons
+    @IBAction func addWaterButton(_ sender: UIButton) {
+        addWater()
+        infoDisplayLable.text = serviceMessage
+    }
+    @IBAction func addMilkButton(_ sender: UIButton) {
+        addMilk()
+        infoDisplayLable.text = serviceMessage
+    }
+    @IBAction func addCoffeeButton(_ sender: UIButton) {
+        addCoffee()
+        infoDisplayLable.text = serviceMessage
+    }
+    @IBAction func clearTrayButton(_ sender: UIButton) {
+        clearGarbage()
+        infoDisplayLable.text = serviceMessage
+    }
 }
 
--- Coffee Machine/service.swift
+++ Coffee Machine/service.swift
@@ -7,23 +7,28 @@
 
 import Foundation
 
-var coffeeTank: Double = 100
-var waterTank: Double = 100
-var milkTank: Double = 100
+var coffeeTank: Double = 0
+var waterTank: Double = 0
+var milkTank: Double = 0
 var garbageTank: Double = 0
+var serviceMessage: String = ""
 
 func addCoffee() {
     coffeeTank += 100
+    serviceMessage = "Coffee added!"
 }
 
 func addWater() {
     waterTank += 500
+    serviceMessage = "Water added!"
 }
 
 func addMilk() {
     milkTank += 300
+    serviceMessage = "Milk added!"
 }
 
 func clearGarbage() {
     garbageTank = 0
+    serviceMessage = "Tank cleaned!"
 }
